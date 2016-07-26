8e9508cad72daf5a9116c55f7deb7eb70e060e40
Si11yGit <benjamin.andrew.clarke174@gmail.com>
2016-07-26 12:24:23 +1200

    Added main and revised Square to interface

--- cluedo/src/board/Square.java
+++ cluedo/src/board/Square.java
@@ -1,11 +1,5 @@
 package board;
 
-public class Square {
-	public enum Type {
-		HALL, BALL_ROOM, KITCHEN, DINING_ROOM,
-		LOUNGE, STUDY, LIBRARY, BILLARD_ROOM,
-		CONSERVATORY, HALLWAY, STARTPOINT
-	}
-	Type type;
+public interface Square {
 
 }
