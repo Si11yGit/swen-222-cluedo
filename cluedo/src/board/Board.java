e60c7b80d3d23ea66be957d216e4e486d7269ec7
henry <henry@henry-Ubuntu-machine>
2016-08-02 11:45:35 +1200

    # WARNING: head commit changed in the meantime
    added methods to main, and some necessary methods to player and board

--- cluedo/src/board/Board.java
+++ cluedo/src/board/Board.java
@@ -1,5 +1,14 @@
 package board;
 
+import java.util.ArrayList;
+
+import parts.Player;
+
 public class Board {
 	Square board[][] = new Square[24][25];
+	ArrayList<Player> players;
+	
+	public Board(ArrayList<Player> players){
+		this.players = players;
+	}
 }
--- cluedo/src/main/Main.java
+++ cluedo/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import board.Board;
 import parts.Card;
@@ -43,6 +44,8 @@ public class Main {
 		public Main(int numPlayers){
 			this.numPlayers=numPlayers;
 			this.allCards = initialiseCards();
+			this.allPlayers = initialisePlayer();
+			this.board = new Board(allPlayers); 
 		}
 
 		
@@ -90,17 +93,88 @@ public class Main {
 
 			return cards;
 		}
-
+		
+		/**
+		 *This method creates the correct solution for the game to compare accusations or suggestions against.  
+		 * @return
+		 */
 		public Solution initialiseSolution(){//a method to initialise solution/generate one
-			new solution();
+			//new Solution();
+			return null;
 		}
 		
+		/**
+		 * A method which initialises the players and creates the correct number of characters to players.
+		 * @return
+		 */
 		public ArrayList<Player> initialisePlayer(){
+			ArrayList<Player> players = new ArrayList<Player>();
+			ArrayList<Character> cards = new ArrayList<Character>();
 			
-		} 
+			//each player will get a character
+			cards.add(new Character("Miss Scarlett"));
+			cards.add(new Character("Colonel Mustard"));
+			cards.add(new Character("Professor Plum"));
+			cards.add(new Character("Reverend Green"));
+			cards.add(new Character("Mrs. White"));
+			cards.add(new Character("Mrs. Peacock"));
+			
+			//use a random number between 0 and 6 to assign a character to a player
+			
+			for(int i = 0;  i< this.numPlayers;i++){
+				//the random number to assign to a player
+				int rand = (int) Math.round(Math.random()*cards.size());
+				if(rand == cards.size()){rand--;}
+				Character card = cards.get(rand);
+				
+				players.add(new Player(i, card));
+				cards.remove(cards.get(rand)); 
+				System.out.println("Player " + i + " you will be  playing as " + card.toString() + ".");
+			}
+			return players;
+		}
 		
 		//place the weapons
+		public void initialiseWeapons(){
+			
+		}
 		
 		//start the game
-
+		public void playGame(){
+			
+		}
+		
+		//deal cards
+		public void dealCards(){
+			while(!this.allCards.isEmpty()){
+				Player player = this.allPlayers.get(this.allCards.size() % this.numPlayers);
+				Card newCard = this.allCards.get(0);
+				player.deal(newCard); //give the current player the top card
+				newCard.setOwner(player);//set the owner of the card to the current player
+				this.allCards.remove(0);//remove the card from the deck
+			}
+			for(int i = 0; i <= this.numPlayers; i++){
+				List<Card> cards = this.allPlayers.get(i).getCards();
+				System.out.println("Your cards are: "+cards.toString());
+			}
+			System.out.println();	
+		}
+		
+		//set game over
+		public void setGameOver(){
+			this.gameOver = true;
+		}
+		
+		//game over, what to do when game is over, declare somebody the winner
+		public void gameOver(){}
+		
+		//get players
+		public int getNumPlayers(){
+			return this.numPlayers;
+		}
+		
+		//get solution
+		public Solution getSolution(){
+			return this.solution;
+		}
 }
