e32c38371443d15f47897e41d6abb0f2135f526c
Si11yGit <benjamin.andrew.clarke174@gmail.com>
2016-08-02 12:54:18 +1200

    added Coordinate to Impassable

--- cluedo/src/board/Impassable.java
+++ cluedo/src/board/Impassable.java
@@ -1,6 +1,15 @@
 package board;
 
 public class Impassable implements Square {
+	private Coordinate position;
+
+	public Impassable(Coordinate position) {
+		this.position = position;
+	}
+
+	public Coordinate getPosition() {
+		return position;
+	}
 
 	@Override
 	public boolean enterable() {
