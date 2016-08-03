75bfded11966cd3ae4f6e09e3d643826f0207c1e
Si11yGit <benjamin.andrew.clarke174@gmail.com>
2016-08-03 15:24:06 +1200

    added comparatability to Coordinate

--- cluedo/src/board/Coordinate.java
+++ cluedo/src/board/Coordinate.java
@@ -1,15 +1,17 @@
 package board;
 
 /**
- * Class which contains an x,y coordinate for use in deciding where a tile is on the board.
+ * Class which contains an x,y coordinate for use in deciding where a tile is on
+ * the board.
+ *
  * @author clarkebenj1
  *
  */
-public class Coordinate {
+public class Coordinate implements Comparable {
 	private int x;
 	private int y;
 
-	public Coordinate (int x, int y) {
+	public Coordinate(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -20,5 +22,26 @@ public class Coordinate {
 
 	public int getY() {
 		return y;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Coordinate) {
+			Coordinate c = (Coordinate) o;
+			if (this.x == c.getX() && this.y == c.getY()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (equals(o)) {
+			return 0;
+		} else {
+			return -1;
+		}
+
 	}
 }
