ebf04be2a1558098669ae517079273b003e8a386
TheWolf174 <clarkie12035@gmail.com>
2016-08-03 22:00:52 +1200

    continued implementation of board

--- cluedo/src/board/Board.java
+++ cluedo/src/board/Board.java
@@ -22,6 +22,9 @@ public class Board {
 	 * the awkward shape and positioning of the rooms
 	 */
 	private void setupBoard() {
+		/*
+		 * takes the positions of each room and creates a room object representing each
+		 */
 		ArrayList<Coordinate> locations = new ArrayList<Coordinate>();
 		for (int y = 1; y < 8; y += 2) {
 			for (int x = 1; x < 14; x += 2) {
@@ -75,14 +78,90 @@ public class Board {
 
 		locations = new ArrayList<Coordinate>();
 		for (int y = 41; y < 48; y += 2) {
-			for (int x = 1; x < 14; x += 2) {
-				if(!((x == 13 && (y == 13 || y == 21)) || (x == 1 && (y == 13 || y == 21)))) {
+			for (int x = 1; x < 12; x += 2) {
+				if(!((x == 11 || x == 1) && y == 47)) {
 					locations.add(new Coordinate(x, y));
 				}
 			}
 		}
 		centre = new Coordinate(7, 18);
 		rooms.put("Conservatory", new Room("Conservatory", centre, locations));
+		
+		locations = new ArrayList<Coordinate>();
+		for (int y = 37; y < 48; y += 2) {
+			for (int x = 17; x < 32; x += 2) {
+				if(!((x == 17 || x == 19 || x == 29 || x == 31) && y == 41)) {
+					locations.add(new Coordinate(x, y));
+				}
+			}
+		}
+		centre = new Coordinate(45, 25);
+		rooms.put("Ballroom", new Room("Ballroom", centre, locations));
+		
+		locations = new ArrayList<Coordinate>();
+		for (int y = 39; y < 48; y += 2) {
+			for (int x = 37; x < 48; x += 2) {
+				if(!(x == 47 && y == 39)) {
+					locations.add(new Coordinate(x, y));
+				}
+			}
+		}
+		centre = new Coordinate(43, 43);
+		rooms.put("Kitchen", new Room("Kitchen", centre, locations));
+		
+		locations = new ArrayList<Coordinate>();
+		for (int y = 21; y < 34; y += 2) {
+			for (int x = 33; x < 48; x += 2) {
+				if(!((x == 33 || x == 35 || x == 37) && y == 34)) {
+					locations.add(new Coordinate(x, y));
+				}
+			}
+		}
+		centre = new Coordinate(40, 27);
+		rooms.put("Dining Room", new Room("Dining Room", centre, locations));
+		
+		/*
+		 * combines the room coordinates into one list for use in hallway creation
+		 */
+		locations = new ArrayList<Coordinate>();
+		for(Room room: rooms.values()) {
+			locations.addAll(room.getPositions());
+		}
+		
+		/*
+		 * sets impassable block in the centre of the board
+		 */
+		for (int y = 17; y < 30; y++) {
+			for (int x = 19; x < 28; x++) {
+				board[x][y] = new Impassable(new Coordinate(x, y));
+			}
+		}
+		
+		/*
+		 * sets impassable square around the exterior
+		 */
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y)); 
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		board[x][y] = new Impassable(new Coordinate(x, y));
+		
+
 
 	}
 
