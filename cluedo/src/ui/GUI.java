bd67ffc22a8bbddbdf45c4e7b14bf1f1e7ebf986
Si11yGit <benjamin.andrew.clarke174@gmail.com>
2016-08-10 15:18:08 +1200

    gui started

--- cluedo/src/ui/GUI.java
+++ cluedo/src/ui/GUI.java
@@ -8,12 +8,13 @@ import board.Board;
 public class GUI {
 	JFrame frame;
 	JButton b1, b2, b3;
-	
+
 	public GUI() {
 		frame = new JFrame();
 		b1 = new JButton("");
+
 	}
-	
+
 	public static void main(String[] args) {
         new GUI();
     }
