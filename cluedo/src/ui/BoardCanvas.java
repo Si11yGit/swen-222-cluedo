89be7f8869eb8543f582148fa793580f39c4e8d1
henry <henry@henry-Ubuntu-machine>
2016-08-18 14:17:34 +1200

    added frame/canvas classes

---
+++ cluedo/src/ui/BoardCanvas.java
@@ -0,0 +1,7 @@
+package ui;
+
+import java.awt.Canvas;
+
+public class BoardCanvas extends Canvas{
+
+}
---
+++ cluedo/src/ui/BoardFrame.java
@@ -0,0 +1,8 @@
+package ui;
+
+import java.awt.event.KeyListener;
+
+public class BoardFrame extends JFrame implements KeyListener, MouseListener, WindowListener{
+	private final BoardCanvas canvas;
+	
+}
