8fb77f28cb2960bd9d183dd7a6e821b316d79c4b
henry <henry@henry-Ubuntu-machine>
2016-08-18 14:21:07 +1200

    removed boardframe

--- cluedo/src/ui/BoardFrame.java
+++
@@ -1,8 +0,0 @@
-package ui;
-
-import java.awt.event.KeyListener;
-
-public class BoardFrame extends JFrame implements KeyListener, MouseListener, WindowListener{
-	private final BoardCanvas canvas;
-	
-}
