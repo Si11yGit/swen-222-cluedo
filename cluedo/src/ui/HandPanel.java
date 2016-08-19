c8f408a87a44126b3d45e939103432de64ba5403
Si11yGit <clarkie12035@gmail.com>
2016-08-19 14:37:36 +1200

    collated gui idea, needs to be fitted to game style though

--- cluedo/src/ui/HandPanel.java
+++ cluedo/src/ui/HandPanel.java
@@ -1,13 +1,58 @@
 package ui;
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import main.Main;
+import parts.Card;
 
+/**
+ *panel that displays the hand of the current player
+ *
+ */
 public class HandPanel extends JPanel {
+	
+	private static final long serialVersionUID = -6086777099635929213L;
+	
+	private Main game;
+	private List<JLabel> cardLabels; 
 
-	public HandPanel(Main game)  {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Create the panel.
+	 */
+	public HandPanel(Main game) {
+		this.game = game;
+		cardLabels = new ArrayList<JLabel>();
+		setLayout(new GridLayout(1, 0, 0, 0));
+		setToolTipText("Current players hand");
+		List<Card> hand = game.getCurrentPlayer().getHand();
+		for(Card c : hand) {
+			ImageIcon card = c.getIcon();
+			
+			JLabel label = new JLabel(card);
+			cardLabels.add(label);
+			add(label);
+		}
 	}
-
+	
+	/**
+	 * update the icon on the labels when a player has moved
+	 */
+	public void updateLabels() {
+		List<Card> newCards = game.getCurrentPlayer().getHand();
+		for(int i = 0; i < cardLabels.size(); i++) {
+			if(i >= newCards.size()){
+				cardLabels.get(i).setIcon(null);
+			}
+			else {
+				cardLabels.get(i).setIcon(newCards.get(i).getIcon());
+			}
+		}
+	}
+	
 }
